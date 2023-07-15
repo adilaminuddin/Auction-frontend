90db471901e27abf4d7f846823d7198740e75ffb
aminuddinadl <adilaminuddin12@gmail.com>
2023-07-15 10:42:27 +0000

    add carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Example from '../components/navbar'
 import Footer from '../components/footer'
+import Slider from '../components/carousel'
 
 export default function Home() {
   return (
     <main className="min-h-screen items-center justify-between p-2">
      <Example />
+      <Slider />
      <Footer />
     </main>
   )
